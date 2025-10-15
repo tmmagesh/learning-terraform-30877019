844d344f9a880c5ed580596872527b05bb4b52a9
tmmagesh <38250934+tmmagesh@users.noreply.github.com>
2025-10-15 09:31:21 -0400

    Copied from Samuelson to make it run in TerraForm

--- main.tf
+++ main.tf
@@ -14,45 +14,30 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" default  {
+data "aws_vpc" "default" {
   default = true
 }
 
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "dev"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["eu-east-1a", "eu-east-1b", "eu-east1c"]
- 
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+resource "aws_instance" "blog" {
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
 
   tags = {
-    Terraform = "true"
-    Environment = "dev"
+    Name = "Learning Terraform"
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
-  vpc_security_group_ids = [module.blog_sg.secuirty_group_id]
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "HelloWorld"
-  }
-module "blog_sg"
-  source ="terraform-aws-modules/secuirty_group/aws"
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id = module.blog_vpc.vpc_id
-  name = "blog"
-  ingree_rules = ["https-443-tcp","http-80-tpc"]
-  ingress_cidr_blocks = ["0.0.0.0/0]
-  egress_rules = ["all-all]
-  egress_cidr_blocks = ["0.0.0.0/0]
-
+  vpc_id  = data.aws_vpc.default.id
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
+
